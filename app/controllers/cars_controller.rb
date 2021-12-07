72c85b3e8e1815f4bbcd75b7e63667224d27cfc4
James Riggleman <james.riggleman@yahoo.com>
2021-12-07 22:11:57 +0800

    finished the car controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,39 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save!
+      # redirect_to car_path(@car)
+      puts "car saved"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @Car.destroy!
+    respond_to do |format|
+      format.html { redirect_to lists_path, notice: "List was successfully destroyed." }
+      format.json { head :no_content }
+    end
+    redirect_to car_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:name, :description, :year, :model, :location)
+  end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,7 @@ class Car < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :year, presence: true, numericality: { only_integer: true, less_than: 2022, greater_than: 1900 }
+  validates :model, presence: true
+  # validates :image, presence: true
+  validates :location, presence: true
 end
